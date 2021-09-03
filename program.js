6d4fc2fe183f22b02c393f61e3b5e3698541502d
Rotem Abir <rotem.abir@gmail.com>
2021-09-03 10:35:42 +0200

    feat: support tag filters in two langauges
    - improved multilingual toggle
    - new support of tag filtering in two languages

--- program.js
+++ program.js
@@ -1,14 +1,28 @@
 import wixData from 'wix-data';
+import wixWindow from 'wix-window';
+
+let myLang;
 
 $w.onReady(function () {
 
-	$w("#datasetCourses").onReady(function () {
+	myLang = wixWindow.multilingual.currentLanguage;
 
-		const  textMaxLength = 260;
+	$w("#datasetCourses").onReady(function () {
+		let textMaxLength = 260;
 		let fullText;
 		let shortText;
 		$w("#repeater").onItemReady(($w, itemData, index)=> {
-  			fullText = itemData.infoEn;
+			if (myLang === "en") {
+  				fullText = itemData.infoEn;
+			}
+			else if (myLang === "he") {
+				$w('#typeTagsLTR').hide();
+				$w('#typeTagsRTL').show();
+				$w('#courseLevel').text = itemData.courseLevelHe;
+				$w('#courseType').text = itemData.courseTypeHe;
+  				fullText = itemData.infoHe;
+				textMaxLength = 225;
+			}
 			if (fullText.length <= textMaxLength) {
 				$w('#courseInfo').text = fullText;
 			}
@@ -21,11 +35,11 @@ $w.onReady(function () {
 
 });
 
-export function typeTags_change(event) {
-	let selectedTags = $w("#typeTags").value;
+export function typeTagsLTR_change(event) {
+	let selectedTags = $w("#typeTagsLTR").value;
 	let results;
 	if(selectedTags.length > 0) {
-		results = wixData.filter().hasSome("tags", selectedTags);
+		results = wixData.filter().hasAll("tags", selectedTags);
 		$w('#datasetCourses').setFilter(results);
 	}
 	else {
@@ -33,3 +47,37 @@ export function typeTags_change(event) {
 		$w('#datasetCourses').setFilter(results);
 	}
 }
+
+export function typeTagsRTL_change(event) {
+	let selectedTags = $w("#typeTagsRTL").value;
+	let result;
+	if(selectedTags.length > 0) {
+		wixData.query("CoursesData")
+			.hasAll("tags", selectedTags)
+			.find()
+			.then( (results) => {
+				if (results.items.length === 0) {
+					$w("#noResaults").show();
+				}
+				else {
+					$w("#noResaults").hide();
+				}
+			});
+		result = wixData.filter().hasAll("tags", selectedTags);
+		$w('#datasetCourses').setFilter(result);
+	}
+	else {
+		result = wixData.filter().isNotEmpty("tags");
+		$w('#datasetCourses').setFilter(result);
+	}
+}
+
+
+/*
+courseType
+courseLevel
+hoursXX
+daysEn
+infoEn
+pauseEn
+*/
