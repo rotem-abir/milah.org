fd2666fd11526875292ff9108782b95f76af594b
Rotem Abir <rotem.abir@gmail.com>
2021-11-24 09:58:06 +0100

    style: better formating

--- faq.js
+++ faq.js
@@ -3,16 +3,15 @@ import wixWindow from 'wix-window';
 let myLang;
 
 $w.onReady(function () {
-
     myLang = wixWindow.multilingual.currentLanguage;
-        if (myLang === 'en') {
-			toggleLang ( $w("#navigationRTL"), $w("#navigationLTR") );
-			toggleLang ( $w("#faqHE"), $w("#faqEN") );
-		}
-        else if (myLang === 'he') {
-			toggleLang ( $w("#navigationLTR"), $w("#navigationRTL") );
-			toggleLang ( $w("#faqEN"), $w("#faqHE") );
-		}
+	if (myLang === 'en') {
+		toggleLang ( $w("#navigationLTR"), $w("#navigationRTL") );
+		toggleLang ( $w("#faqHE"), $w("#faqEN") );
+	}
+	else if (myLang === 'he') {
+		toggleLang ( $w("#navigationRTL"), $w("#navigationLTR") );
+		toggleLang ( $w("#faqEN"), $w("#faqHE") );
+	}
 });
 
 function toggleLang (hideMe, showMe) {
