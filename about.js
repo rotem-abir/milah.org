7685212d48b9162801fc55fd5c19492256df17f8
Rotem Abir <rotem.abir@gmail.com>
2021-12-14 16:30:58 +0100

    fix: bug in navigation bar
    bug fix for multilingual support in navigation bar

--- about.js
+++ about.js
@@ -2,17 +2,17 @@ import wixWindow from 'wix-window';
 
 let myLang;
 
+function toggleLang (hideMe, showMe) {
+	hideMe.hide();
+	showMe.show();
+}
+
 $w.onReady(function () {
 	myLang = wixWindow.multilingual.currentLanguage;
 	if (myLang === "en") {
 		toggleLang ( $w("#navigationLTR"), $w("#navigationRTL") );
 	}
 	else if (myLang === "he") {
-		toggleLang ( $w("#navigationRTL"), $w("#navigationLTR") );
+        toggleLang ( $w("#navigationRTL"), $w("#navigationLTR") );
 	}
 });
-
-function toggleLang (hideMe, showMe) {
-	hideMe.hide();
-	showMe.show();
-}
